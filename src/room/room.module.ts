69fb8fc4880bdbe5e96c8df78f2ff833aa3f8975
ritheasen <ritheasenn@gmail.com>
2023-07-18 18:25:20 +0700

    Room module

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 
 @Module({
   controllers: [RoomController],
